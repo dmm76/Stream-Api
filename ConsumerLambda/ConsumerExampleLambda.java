790a58955d3cfddf8468d052aeb7a18b1ae4a993
Douglas Marcelo Monquero <douglas.monquero@gmail.com>
2024-10-08 18:32:08 -0300

    Lambda Update

--- ConsumerLambda/ConsumerExampleLambda.java
+++ ConsumerLambda/ConsumerExampleLambda.java
@@ -7,10 +7,13 @@ public class ConsumerExampleLambda {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
 
        //Usar o Consumer para imprimir numero pare no Stream
-        numeros.stream().forEach(n -> {
-            if(n % 2 == 0){
-                System.out.println(n);
-            }
-        });
+        //numeros.stream().forEach(n -> {
+        //    if(n % 2 == 0){
+        //        System.out.println(n);
+        //    }
+        //});
+        numeros.stream()
+        .filter(n -> n % 2 == 0)
+        .forEach(System.out::println);
     }
 }
