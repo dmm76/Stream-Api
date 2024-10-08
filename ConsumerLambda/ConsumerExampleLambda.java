c74f619029d3c411c7535bd262064d78112fede8
Douglas Marcelo Monquero <douglas.monquero@gmail.com>
2024-10-08 17:19:58 -0300

    ajustes

--- ConsumerLambda/ConsumerExampleLambda.java
+++ ConsumerLambda/ConsumerExampleLambda.java
@@ -1,8 +1,5 @@
-package ConsumerLambda;
-
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class ConsumerExampleLambda {
     public static void main(String[] args) {
