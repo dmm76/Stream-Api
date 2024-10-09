6a8e91c17c72dc7354a9f9b118bdc902d503fc91
Douglas Marcelo Monquero <douglas.monquero@gmail.com>
2024-10-08 22:36:48 -0300

    final

---
+++ teste.java
@@ -0,0 +1,9 @@
+import java.util.Optional;
+
+public class teste {
+    public static void main(String[] args){
+        Optional<String> optionalValue = Optional.ofNullable(null);
+        String result = optionalValue.orElse("Default");
+        System.out.println(result);
+    }
+}
